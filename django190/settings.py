6c87da8cb8fa22dd8b203013a8819e4b14554f3a
pinisetti-lalith <63152024+pinisetti-lalith@users.noreply.github.com>
2021-05-28 11:23:41 +0530

    Update settings.py

--- django190/settings.py
+++ django190/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ds34s@=sawu_tmc_c7nos3%!_e3jjc0@=4^92yech#5r_uc38&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
